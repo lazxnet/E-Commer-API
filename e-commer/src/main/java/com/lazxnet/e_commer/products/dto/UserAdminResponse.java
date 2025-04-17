c9809c65fbea71b9de9579583e80e12944c28976
Lazaro Campos <lazaromanuel65@gmail.com>
2025-04-17 16:15:02 -0400

    [REFACTOR]  respuesta de userAdmin en Product

--- e-commer/src/main/java/com/lazxnet/e_commer/products/dto/UserAdminResponse.java
+++ e-commer/src/main/java/com/lazxnet/e_commer/products/dto/UserAdminResponse.java
@@ -2,12 +2,9 @@ package com.lazxnet.e_commer.products.dto;
 
 import lombok.Data;
 
-import java.util.UUID;
 
 @Data
 public class UserAdminResponse {
-
-    private UUID userAdminId;
 
     private String fullName;
 
--- e-commer/src/main/java/com/lazxnet/e_commer/userAdmin/dto/UserAdminResponseDTO.java
+++ e-commer/src/main/java/com/lazxnet/e_commer/userAdmin/dto/UserAdminResponseDTO.java
@@ -2,11 +2,9 @@ package com.lazxnet.e_commer.userAdmin.dto;
 
 import lombok.Data;
 
-import java.util.UUID;
 
 @Data
 public class UserAdminResponseDTO {
-    //private UUID userAdminId;
     private String email;
     private String fullName;
 }
