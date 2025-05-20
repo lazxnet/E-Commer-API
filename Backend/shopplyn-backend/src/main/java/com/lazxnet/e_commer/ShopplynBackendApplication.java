d380b972ddea1a140ffa60a32737817750f3a6b4
Lázaro Campos <lazaromanuel65@gmail.com>
2025-05-20 08:11:35 -0500

    Update ShopplynBackendApplication.java

--- Backend/shopplyn-backend/src/main/java/com/lazxnet/e_commer/ShopplynBackendApplication.java
+++ Backend/shopplyn-backend/src/main/java/com/lazxnet/e_commer/ShopplynBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ECommerApplication {
+public class ShopplynBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ECommerApplication.class, args);
+		SpringApplication.run(ShopplynBackendApplication.class, args);
 	}
 
 }
